8af93e59e1e34482c6469c36bf53ba456094319a
g.timokhin <g.timokhin@sptlab.ru>
2025-04-05 22:57:36 +0300

    rewrote test_add_figures -- removed match/case

--- tests/test_add_figures.py
+++ tests/test_add_figures.py
@@ -1,81 +1,30 @@
 import pytest
-import math
 
 from src.triangle import Triangle
 from src.circle import Circle
 from src.rectangle import Rectangle
 from src.square import Square
-from src.trigonometry import tri_area
 
 
 @pytest.mark.regress
 @pytest.mark.positive
-@pytest.mark.parametrize('figures',
-                         ['circle&rectangle',
-                          'circle&square',
-                          'circle&triangle',
-                          'rectangle&square',
-                          'rectangle&triangle',
-                          'square&triangle'])
-def test_add_figures(figures):
-    match figures:
-        case 'circle&rectangle':
-            radius = 3
-            circle = Circle(radius=radius)
-            circle_area = round(math.pi * radius ** 2, 2)
-            side_a, side_b = 3, 4
-            rectangle = Rectangle(side_a, side_b)
-            rectangle_area = side_a * side_b
-            assert circle.add_area(rectangle) == circle_area + rectangle_area
-        case 'circle&square':
-            radius = 3
-            circle = Circle(radius=radius)
-            circle_area = round(math.pi * radius ** 2, 2)
-            side = 3
-            square = Square(side=side)
-            square_area = side ** 2
-            assert circle.add_area(square) == circle_area + square_area
-        case 'circle&triangle':
-            radius = 3
-            circle = Circle(radius=radius)
-            circle_area = round(math.pi * radius ** 2, 2)
-            side_a, side_b, side_c = 3, 4, 5
-            triangle = Triangle(side_a=side_a,
-                                side_b=side_b,
-                                side_c=side_c)
-            triangle_area = tri_area(side_a=side_a,
-                                     side_b=side_b,
-                                     side_c=side_c)
-            assert triangle.add_area(circle) == circle_area + triangle_area
-        case 'rectangle&square':
-            side_a, side_b = 3, 4
-            rectangle = Rectangle(side_a, side_b)
-            rectangle_area = side_a * side_b
-            side = 3
-            square = Square(side=side)
-            square_area = side ** 2
-            assert rectangle.add_area(square) == rectangle_area + square_area
-        case 'rectangle&triangle':
-            side_a, side_b = 3, 4
-            rectangle = Rectangle(side_a, side_b)
-            rectangle_area = side_a * side_b
-            side_a, side_b, side_c = 3, 4, 5
-            triangle = Triangle(side_a=side_a,
-                                side_b=side_b,
-                                side_c=side_c)
-            triangle_area = tri_area(side_a=side_a,
-                                     side_b=side_b,
-                                     side_c=side_c)
-            assert rectangle.add_area(triangle) == rectangle_area + triangle_area
-        case 'square&triangle':
-            side = 3
-            square = Square(side=side)
-            square_area = side ** 2
-            side_a, side_b, side_c = 3, 4, 5
-            triangle = Triangle(side_a=side_a,
-                                side_b=side_b,
-                                side_c=side_c)
-            triangle_area = tri_area(side_a=side_a,
-                                     side_b=side_b,
-                                     side_c=side_c)
-            assert square.add_area(triangle) == square_area + triangle_area
+@pytest.mark.parametrize('figure_1, figure_2',
+                         [
+                             (Circle(3), Rectangle(3, 4)),
+                             (Circle(3), Square(3)),
+                             (Circle(3), Triangle(3, 4, 5)),
+                             (Rectangle(3, 4), Square(3)),
+                             (Rectangle(3, 4), Triangle(3, 4, 5)),
+                             (Square(3), Triangle(3, 4, 5))
+                         ],
+                         ids=[
+                                'Circle and Rectangle',
+                                'Circle and Square',
+                                'Circle and Triangle',
+                                'Rectangle and Square',
+                                'Rectangle and Triangle',
+                                'Square and Triangle'
+                             ]
+                         )
+def test_add_figures(figure_1, figure_2):
+    assert figure_1.add_area(figure_2) == figure_1.get_area + figure_2.get_area
